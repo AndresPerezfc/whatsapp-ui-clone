89c3751e1a1c5a9d2f1d562268e699408d14fc5c
andresperezfc <felipe3apc@gmail.com>
2022-01-14 20:52:13 -0500

    Diseño de la lista de contactos para web

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widgets/constacs_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Este es web")),
-    );
+    return Scaffold(
+        body: Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //Web Profile Bar
+                //Web Search Bar
+                ContactsList()
+              ],
+            ),
+          ),
+        ),
+        // Web screen
+        Container(
+          width: MediaQuery.of(context).size.width * 0.75,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/backgroundImage.png'),
+                  fit: BoxFit.cover)),
+        )
+      ],
+    ));
   }
 }
